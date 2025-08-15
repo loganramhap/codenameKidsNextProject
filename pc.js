0597032ccf6fc76f5edb6dcd694e9e45dba5c2d2
Logan Ramhap <44788545+loganramhap@users.noreply.github.com>
2025-08-15 17:28:41 -0400

    Add codename generator web app
    Introduces a simple web app to generate random project or operation codenames. Includes HTML structure, CSS styling, and JavaScript logic for generating and switching codename types.

---
+++ pc.js
@@ -0,0 +1,98 @@
+var attributes = [
+	// Environ
+	"desert", "tundra", "mountain", "space", "field", "urban",
+	// Stealth and cunning
+	"hidden", "covert", "uncanny", "scheming", "decisive", "untouchable", "stalking",
+	// Volitility
+	"rowdy", "dangerous", "explosive", "threatening", "warring", "deadly", "killer", "insane", "wild",
+	// Needs correction
+	"bad", "unnecessary", "unknown", "unexpected", "waning",
+	// Organic Gems and materials
+	"amber", "bone", "coral", "ivory", "jet", "nacre", "pearl", "obsidian", "glass",
+	// Regular Gems
+	"agate", "beryl", "diamond", "opal", "ruby", "onyx", "sapphire", "emerald", "jade",
+	// Colors
+	"red", "orange", "yellow", "green", "blue", "violet",
+	// Unsorted
+	"draconic", "wireless", "spinning", "falling", "orbiting", "hunting", "chasing", "searching", "revealing", "flying", "destroyed", "inconceivable", "tarnished"
+]
+
+var objects = [
+	// Large cats
+	"panther", "wildcat", "tiger", "lion", "cheetah", "cougar", "leopard",
+	// Snakes
+	"viper", "cottonmouth", "python", "boa", "sidewinder", "cobra",
+	// Other predators
+	"grizzly", "jackal", "falcon",
+	// Prey
+	"wildebeest", "gazelle", "zebra", "elk", "moose", "deer", "stag", "pony", "koala", "sloth",
+	// HORSES!
+	"horse", "stallion", "foal", "colt", "mare", "yearling", "filly", "gelding",
+	// Mythical creatures
+	"mermaid", "unicorn", "fairy", "troll", "yeti", "pegasus", "griffin", "dragon",
+	// Occupations
+	"nomad", "wizard", "cleric", "pilot", "captain", "commander", "general", "major", "admiral", "chef", "inspector", 
+	// Technology
+	"mainframe", "device", "motherboard", "network", "transistor", "packet", "robot", "android", "cyborg", "display", "battery", "memory", "disk", "cartridge", "tape", "camera", "projector",
+	// Sea life
+	"octopus", "lobster", "crab", "barnacle", "hammerhead", "orca", "piranha",
+	// Weather
+	"storm", "thunder", "lightning", "rain", "hail", "sun", "drought", "snow", "drizzle",
+	// Musical
+	"piano", "keyboard", "guitar", "trumpet", "trombone", "flute", "cornet", "horn", "tuba", "clarinet", "saxophone", "piccolo", "violin", "harp", "cello", "drum", "organ", "banjo", "rhythm", "beat", "sound", "song",
+	// Tools
+	"screwdiver", "sander", "lathe", "mill", "welder", "mask", "hammer", "drill", "compressor", "wrench", "mixer", "router", "vacuum",
+	// Other
+	"warning", "presence", "weapon", "player", "ink", "case", "cup", "chain", "door"
+]
+
+const newCodenameElem = document.getElementById('newProject');
+const switchElem = document.getElementById('switchType');
+
+let type = "PROJECT";
+let codename = "CODENAME";
+
+function createCodename()
+{
+	const elem = document.getElementById('codename');
+	elem.classList.add('updating');
+	
+	setTimeout(() => {
+		const f = attributes[Math.floor(Math.random() * attributes.length)].toUpperCase();
+		const l = objects[Math.floor(Math.random() * objects.length)].toUpperCase();
+		codename = `${f} ${l}`;
+		elem.innerText = codename;
+		elem.classList.remove('updating');
+	}, 150);
+}
+
+function switchType()
+{
+	const titleElem = document.getElementById('opType');
+	if (type === "PROJECT")
+	{
+		type = "OPERATION";
+		switchElem.innerText = `Switch to Project`;
+	}
+	else
+	{
+		type = "PROJECT";
+		switchElem.innerText = `Switch to Operation`;
+	}
+
+	titleElem.innerText = type;
+}
+
+
+
+createCodename();
+
+newCodenameElem.onclick = (e) => {
+	e.preventDefault();
+	createCodename();
+}
+
+switchElem.onclick = (e) => {
+	e.preventDefault();
+	switchType();
+}
